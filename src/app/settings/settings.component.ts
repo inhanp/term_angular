65b9b811cef7897a5b25e58203067827892152c6
Inhan Park <inhan.park.95@gmail.com>
2021-08-05 00:40:02 +0900

    Setting

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,9 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../_models/user';
+import {UserService} from '../_services/user.service';
+import {AuthService} from '../_services/auth.service';
 
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+  currentUser: User;
+  caloriegoal: number;
+  minutegoal: number;
+  constructor(private userService: UserService,
+              private authService: AuthService) {}
 
+  ngOnInit() {
+    this.currentUser = this.authService.currentUserValue;
+    console.log('setting executed');
+    this.caloriegoal = 2500; this.minutegoal = 100;
+    console.log(this.currentUser.username);
+    this.currentUser.caloriegoal = this.caloriegoal; this.currentUser.minutegoal = this.minutegoal;
+    this.userService.setgoals(this.currentUser);
+  }
 
-
-
-
-
-
-//TODO: since you will be creating the 'settings' component from scratch, you should delete the folder containing the component files (css,html and ts) and do 'ng g c settings --skipTests'.
+}
